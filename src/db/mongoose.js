29442b4ad8fc08875e78d2175dea0de26c820fc7
sanchit36 <sbhadgal_be19@thapar.edu>
2021-08-22 09:19:20 +0530

    testing the node app with jest

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 mongoose
   .connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
-    useCreateIndexes: true,
   })
   .then(() => {
     console.log("Connected to MongoDB");
--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
-const express = require("express");
-require("./db/mongoose");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
+const app = require("./app");
 
-const app = express();
 const PORT = process.env.PORT;
-
-// Express config
-app.use(express.json());
-
-// Routes
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(PORT, () => {
   console.log("Server is listening on port " + PORT);
